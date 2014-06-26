6dc51460ea9a19a03f48a57a2d687f99dcfceebc
toalexjin <toalexjin@hotmail.com>
2014-06-26 15:46:54 +0800

    Add first version

---
+++ src/main/resources/archetype-resources/src/main/java/HelloWorld.java
@@ -0,0 +1,26 @@
+package ${package};
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet("/")
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = -3721191680245393618L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain");
+
+		PrintWriter writer = response.getWriter();
+		writer.println("Hello, World!");
+	}
+}
+
